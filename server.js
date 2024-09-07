2834c701baddadd1a9f267b0de48a018dff11ba1
shtrv <71714403+shtrv@users.noreply.github.com>
2024-09-07 18:13:46 +0300

    added gitignore, minor changes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+/* const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) => {
@@ -8,4 +8,4 @@ const server = http.createServer((req, res) => {
 })
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
-})
+}) */
